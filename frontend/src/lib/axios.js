4fcec84feaaecaf6c83bcd39bd85dc9adca5dd63
Dipesh Shah <118037697+urstrulydipesh@users.noreply.github.com>
2025-08-27 16:52:05 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,20 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:5001/api"
+//       : import.meta.env.VITE_BACKEND_URL + "/api",
+//   withCredentials: true,
+// });
+
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : import.meta.env.VITE_BACKEND_URL + "/api",
+      : "https://chat-app-backend-ka8x.onrender.com/api",
   withCredentials: true,
 });
