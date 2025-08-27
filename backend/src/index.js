da50e7989b91ca34d5df4bc5f5a1e86fa7c0a26d
urstrulydipesh <dipesh.shah546@gmail.com>
2025-08-28 00:11:37 +0530

    cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,8 @@ app.use(
   cors({
     origin: "*",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
